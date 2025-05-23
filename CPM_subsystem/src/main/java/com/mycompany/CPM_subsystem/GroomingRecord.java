29d095978c7e50820f421adedb86c107d184f612
pradeep228 <A30427656@students.iibit.edu.au>
2025-05-23 17:39:49 +0530

    Reviewed and improved version of subsystem with comments applied

--- CPM_subsystem/src/main/java/com/mycompany/CPM_subsystem/GroomingRecord.java
+++ CPM_subsystem/src/main/java/com/mycompany/CPM_subsystem/GroomingRecord.java
@@ -1,65 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.CPM_subsystem;
 
 import java.util.Date;
-
+// Grooming Record class to keep track of grooming for pets
 public class GroomingRecord {
     private String recordID;
     private Date date;
     private String serviceType;
     private String groomerNotes;
-
-    // Constructor
-    public GroomingRecord(String recordID, Date date, String serviceType, String groomerNotes) {
-        this.recordID = recordID;
-        this.date = date;
-        this.serviceType = serviceType;
-        this.groomerNotes = groomerNotes;
-    }
-
-    // Getter and Setter methods
-    public String getRecordID() {
-        return recordID;
-    }
-
-    public void setRecordID(String recordID) {
-        this.recordID = recordID;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getServiceType() {
-        return serviceType;
-    }
-
-    public void setServiceType(String serviceType) {
-        this.serviceType = serviceType;
-    }
-
-    public String getGroomerNotes() {
-        return groomerNotes;
-    }
-
-    public void setGroomerNotes(String groomerNotes) {
-        this.groomerNotes = groomerNotes;
-    }
-
-    // Method to view grooming history
-    public void viewGroomingHistory() {
-        System.out.println("Grooming Record ID: " + recordID);
-        System.out.println("Date: " + date.toString());
-        System.out.println("Service Type: " + serviceType);
-        System.out.println("Groomer Notes: " + groomerNotes);
-    }
-
-    // toString method to display GroomingRecord details
-    @Override
-    public String toString() {
-        return "GroomingRecord [recordID=" + recordID + ", date=" + date + ", serviceType=" + serviceType + ", groomerNotes=" + groomerNotes + "]";
-    }
+// Method for this class
+    public void viewGroomingHistory() {}
 }
